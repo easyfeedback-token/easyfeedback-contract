99f8ba7db190019907b3c64f96af83f93183f790
Enrique Berrueta <eabz@polispay.org>
2021-01-21 11:27:34 -0600

    EasyF update

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,6 @@
 const Token = artifacts.require('EasyFeedBackToken.sol');
 
 module.exports = async function (deployer, _network, addresses) {
-  // Deploy BambooToken if not on testnet
   console.log("Deploying token...")
   await deployer.deploy(Token);
   const token = await Token.deployed()
